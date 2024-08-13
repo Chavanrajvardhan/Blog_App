ffca033144d835163f745a7ae7d825f946236a2a
Raj <112115798+Chavanrajvardhan@users.noreply.github.com>
2024-08-13 16:39:37 +0530

    make new components button input

---
+++ Blog_App/src/components/Header/Headre.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Container, LogoutBtn} from '../index.js'
+import Link from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+
+function Headre() {
+    return (
+        <div>Header</div>
+    )
+}
+
+export default Headre
---
+++ Blog_App/src/components/index.js
@@ -0,0 +1,12 @@
+import Headre from "./Header/Headre.jsx";
+import Footer from "./Footer/Footer.jsx";
+import LogoutBtn from "./Header/LogoutBtn.jsx";
+import Container from "./container/Container.jsx";
+
+
+export {
+    Headre,
+    Footer,
+    LogoutBtn,
+    Container
+}
